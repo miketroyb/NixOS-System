f7167a41c5634d57017236bd39c60e3166fc7aac
michael <michaeltblount@pm.me>
2024-02-16 11:52:54 -0600

    enabled nixos stable again and split system between stable vs unstable

--- flake.nix
+++ flake.nix
@@ -1,19 +1,19 @@
 {
   description = "My NixOS workstation configurationuration";
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    # home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager-unstable.url = "github:nix-community/home-manager/master";
-    #home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
   outputs = { 
   self, 
-  #nixpkgs, 
+  nixpkgs, 
   nixpkgs-unstable,
-  #home-manager, 
+  home-manager, 
   home-manager-unstable, 
   ... 
   }@inputs: {
@@ -32,6 +32,23 @@
             home-manager.users.anon = {
               imports = [ ./modules/home-manager/main.nix ];
               home.stateVersion = "24.05";
+            };
+          }
+        ];
+      };
+      NixOS-stable = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./hardware/_main.nix
+	  ./configuration.nix
+	  ./modules/system/sysver-stable.nix
+	  ./modules/system/fonts.nix
+	  ./modules/system/syspkgs.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.anon = {
+              imports = [ ./modules/home-manager/main.nix ];
+              home.stateVersion = "23.11";
             };
           }
         ];
