a39d11c7f83ce336d73b35650446e53cafabb443
michael <michaeltblount@pm.me>
2024-02-20 11:09:24 -0600

    removed pkgs prefix, easier way without.

--- modules/home-manager/main.nix
+++ modules/home-manager/main.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs,  ... }:
 
 { 
   home.username = "anon";
@@ -6,45 +6,45 @@
   programs.home-manager.enable = true;
   fonts.fontconfig.enable = false;
 
-  home.packages = [ 
+  home.packages = with pkgs; [ 
   	## Desktop agnostic apps
-  	pkgs.keepassxc pkgs.ungoogled-chromium 
-        pkgs.firefox pkgs.megasync pkgs.libreoffice 
-        pkgs.ranger pkgs.fd pkgs.fzf pkgs.git 
-        pkgs.wl-clipboard pkgs.flatpak
-        pkgs.libsForQt5.okular pkgs.thunderbird pkgs.google-chrome
-        pkgs.openrgb-with-all-plugins pkgs.blackbox-terminal 
-        pkgs.protonmail-bridge pkgs.rofi
-        pkgs.gimp-with-plugins pkgs.htop
-	pkgs.freetube
+  	keepassxc ungoogled-chromium 
+        firefox megasync libreoffice 
+        ranger fd fzf git 
+        wl-clipboard flatpak
+        libsForQt5.okular thunderbird google-chrome
+        openrgb-with-all-plugins blackbox-terminal 
+        protonmail-bridge rofi
+        gimp-with-plugins htop
+	freetube
 	## Gnome specific apps
-        pkgs.gnome.gnome-tweaks pkgs.adw-gtk3
-        pkgs.gnome.gnome-software pkgs.gnome-extension-manager
-        pkgs.gnomeExtensions.paperwm 
-        pkgs.gnomeExtensions.unmess 
-        pkgs.gnomeExtensions.workspaces-indicator-by-open-apps 
-        pkgs.gnomeExtensions.window-state-manager 
-        pkgs.gnomeExtensions.dash-to-panel 
-        pkgs.gnomeExtensions.extension-list 
-        pkgs.gnomeExtensions.night-theme-switcher 
-        pkgs.gnomeExtensions.custom-accent-colors 
-        pkgs.gnomeExtensions.caffeine 
-        pkgs.gnomeExtensions.blur-my-shell 
-        pkgs.gnomeExtensions.bluetooth-quick-connect 
-        pkgs.gnomeExtensions.alphabetical-app-grid 
-        pkgs.gnomeExtensions.gsconnect
-        pkgs.gnomeExtensions.pop-shell
-        pkgs.gnomecast pkgs.qgnomeplatform
+        gnome.gnome-tweaks adw-gtk3
+        gnome.gnome-software gnome-extension-manager
+        gnomeExtensions.paperwm 
+        gnomeExtensions.unmess 
+        gnomeExtensions.workspaces-indicator-by-open-apps 
+        gnomeExtensions.window-state-manager 
+        gnomeExtensions.dash-to-panel 
+        gnomeExtensions.extension-list 
+        gnomeExtensions.night-theme-switcher 
+        gnomeExtensions.custom-accent-colors 
+        gnomeExtensions.caffeine 
+        gnomeExtensions.blur-my-shell 
+        gnomeExtensions.bluetooth-quick-connect 
+        gnomeExtensions.alphabetical-app-grid 
+        gnomeExtensions.gsconnect
+        gnomeExtensions.pop-shell
+        gnomecast qgnomeplatform
 	## kde specific apps
-	pkgs.latte-dock
-	pkgs.libsForQt5.skanlite pkgs.libsForQt5.accounts-qt
-        pkgs.libsForQt5.skanpage pkgs.libsForQt5.filelight 
-        pkgs.libsForQt5.kgpg libsForQt5.merkuro
-        pkgs.gnupg pkgs.libsForQt5.kcalc
-        pkgs.libsForQt5.kaccounts-integration
-        pkgs.libsForQt5.kaccounts-providers
-        pkgs.libsForQt5.kio-gdrive
-        pkgs.libsForQt5.okular
+	latte-dock
+	libsForQt5.skanlite libsForQt5.accounts-qt
+        libsForQt5.skanpage libsForQt5.filelight 
+        libsForQt5.kgpg libsForQt5.merkuro
+        gnupg libsForQt5.kcalc
+        libsForQt5.kaccounts-integration
+        libsForQt5.kaccounts-providers
+        libsForQt5.kio-gdrive
+        libsForQt5.okular
     ];
 }
 
